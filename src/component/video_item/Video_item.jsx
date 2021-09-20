89cce310433ea361c9bba55d16704fc620b48acb
journy002 <journy002@naver.com>
2021-09-20 13:49:22 +0900

    new update

--- src/component/video_item/Video_item.jsx
+++ src/component/video_item/Video_item.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import styles from './video_item.module.css'
 
-const VideoItem = (props) => <h1>{props.video.snippet.title}</h1>
+
+// {video: {snippet}} =》 원래 {video}였던걸 video안에 snippet을 받아올거다 라는 의미로 사용가능
+// 위처럼 비디오 안에 스닙쳇을 쓴다 설정해주면 props를 사용할때 snippet부터 써주면 오케이!
+const VideoItem = ({video: {snippet}}) => 
+<li className={styles.container}>
+    <div className={styles.video}>
+        <img
+        className={styles.thumbnail}
+        src={snippet.thumbnails.medium.url}
+        alt='video thumnail'
+        />
+        <div className={styles.metadata}>
+            <p className={styles.title} >{snippet.title}</p>
+            <p className={styles.channel} >{snippet.channelTitle}</p>
+        </div>
+    </div>
+</li>
 
 export default VideoItem;
